dc764a849dfd461a0a92e1e106a6eeaa897130fa
cristianiniguez <felizpc1@gmail.com>
2022-03-22 21:03:06 -0400

    created method to create users

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 import { User } from '../entities/user.entity';
+import { CreateUserDTO } from '../dtos/users.dto';
 
 @Injectable()
 export class UsersService {
@@ -20,6 +22,20 @@ export class UsersService {
 
   findByEmail(email: string) {
     return this.userModel.findOne({ email }).exec();
+  }
+
+  async create(data: CreateUserDTO) {
+    const newUser = new this.userModel(data);
+
+    // TODO: check if email is already used
+
+    const hashedPassword = await bcrypt.hash(newUser.password, 10);
+    newUser.password = hashedPassword;
+
+    const savedUser = await newUser.save();
+
+    const { password: _, ...user } = savedUser.toJSON();
+    return user;
   }
 
   async remove(id: string) {
