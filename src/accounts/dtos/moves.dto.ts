cc055515380dbde009e7d29ea01911fb48ac2a3d
cristianiniguez <felizpc1@gmail.com>
2022-04-06 20:00:49 -0400

    class validator installed and applied to account dtos

--- src/accounts/dtos/moves.dto.ts
+++ src/accounts/dtos/moves.dto.ts
@@ -1,9 +1,38 @@
+import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsDate,
+  IsIn,
+  IsMongoId,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+
 import { MoveType } from '../models/move.model';
 
 export class CreateMoveDTO {
+  @IsString()
+  @IsNotEmpty()
   detail: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
   amount: number;
+
+  @IsDate()
+  @IsNotEmpty()
   date: Date;
+
+  @IsString()
+  @IsIn([MoveType.INCOME, MoveType.OUTCOME])
+  @IsNotEmpty()
   type: MoveType;
+
+  @IsMongoId()
+  @IsNotEmpty()
   account: string;
 }
+
+export class UpdateMoveDTO extends PartialType(CreateMoveDTO) {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      whitelist: true,
+    }),
+  );
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
